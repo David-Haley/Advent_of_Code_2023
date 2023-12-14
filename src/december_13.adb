16f70e01bf5391c32b0a151b75cb88b51fad596d
David-Haley <david.haley@bigpond.com>
2023-12-14 10:53:01 +1000

    Day 13, Part 1 correct, Part 2 work in progress, gets correct answer for example but too low for my input

--- src/december_13.adb
+++ src/december_13.adb
@@ -31,6 +31,12 @@ procedure December_13 is
 
    package Mirror_Lists is new Ada.Containers.Doubly_Linked_Lists (Ordinates);
 
+   type Smudges is record
+      Object, Reflection, Position : Ordinates;
+   end record; -- Smudges
+
+   package Smudge_Lists is new Doubly_Linked_Lists (Smudges);
+
    procedure Read_input (Patch_Store : out Patch_Stores.List) is
 
       Input_File : File_Type;
@@ -108,13 +114,64 @@ procedure December_13 is
       end loop; -- Mc /= Mirror_Lists.No_Element and not Found
    end Find_Mirror;
 
+   procedure  Find_Mirror_2 (Vector : in Vectors.Map;
+                             Found : out Boolean;
+                             Position : out Ordinates) is
+
+      use Smudge_Lists;
+      Difference : Lines.Set;
+      Vn : Vectors.Cursor;
+      Smudge_List : Smudge_Lists.List := Smudge_Lists.Empty_List;
+      Sc : Smudge_Lists.Cursor;
+      O, R : Ordinates;
+
+   begin -- Find_Mirror_2
+      for V in Iterate (Vector) loop
+         Vn := Next (V);
+         while Vn /= Vectors.No_Element loop
+            Difference := Symmetric_Difference (Element (V), Element (Vn));
+            if Length (Difference) = 1 then
+               Append (Smudge_List, (Object => Key (V),
+                                     Reflection => Key (Vn),
+                                     Position => First_Element (Difference)));
+            end if; -- Length (Difference) = 1
+            Next (Vn);
+         end loop; -- Vn /= Vectors.No_Element
+      end loop; -- V in Iterate (Vector)
+      Sc := First (Smudge_List);
+      Found := False;
+      while Sc /= Smudge_Lists.No_Element and not Found loop
+         Found := True;
+         Position := (Element (Sc).Object + Element (Sc).Reflection) / 2;
+         O := Position;
+         R := Position + 1;
+         loop -- Test reflection
+            If Contains (Vector, O) and Contains (Vector, R) then
+               Difference := Symmetric_Difference (Element (Vector, O),
+                                                   Element (Vector, R));
+               Found := @ and (Element (Vector, O) = Element (Vector, R)
+                               or else (O = Element (Sc).Object and
+                                          R = Element (Sc).Reflection and
+                                          First_Element (Difference) =
+                                          Element (Sc).Position));
+            end if; -- Contains (Vector, O) and Contains (Vector, R)
+            exit when O = 1;
+            O := @ - 1;
+            R := @ + 1;
+         end loop; -- Test reflection
+         Next (Sc);
+      end loop; -- Sc /= Smudge_Lists.No_Element and not Found
+   end  Find_Mirror_2;
+
    Patch_Store : Patch_Stores.List;
    Found : Boolean;
    Position : Ordinates;
-   Sum : Natural := 0;
+   Sum : Natural;
+   Cleaned_Patch : Patches;
 
 begin -- December_13
    Read_input (Patch_Store);
+   Sum := 0;
    for P in Iterate (Patch_Store) loop
       Find_Mirror (Patch_Store (P).Row, Found, Position);
       if Found then
@@ -127,6 +184,17 @@ begin -- December_13
    end loop; -- P in Iterate (Patch_Store)
    Put_Line ("Part one:" & Sum'Img);
    DJH.Execution_Time.Put_CPU_Time;
-   Put_Line ("Part two:");
+   Sum := 0;
+   for P in Iterate (Patch_Store) loop
+      Find_Mirror_2 (Patch_Store (P).Row, Found, Position);
+      if Found then
+         Sum := @ + 100 * Position;
+      end if; -- Found
+      Find_Mirror_2 (Patch_Store (P).Column, Found, Position);
+      if Found then
+         Sum := @ + Position;
+      end if; -- Found
+   end loop; -- P in Iterate (Patch_Store)
+   Put_Line ("Part two:" & Sum'Img);
    DJH.Execution_Time.Put_CPU_Time;
 end December_13;
