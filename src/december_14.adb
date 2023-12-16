d79bc9a3678065236c53f7d216db2755c90cfc0f
David-Haley <david.haley@bigpond.com>
2023-12-16 21:57:47 +1000

    Day 14 Work in progress

--- src/december_14.adb
+++ src/december_14.adb
@@ -30,6 +30,13 @@ procedure December_14 is
 
    type Directions is (North, West, South, East);
 
+   package Round_Sets is new Ada.Containers.Ordered_Sets (Coordinates);
+
+   subtype Cycles is Natural range Natural'First .. 1000000000;
+
+   package Cycle_Maps is new
+     Ada.Containers.Ordered_Maps (Cycles, Round_Sets.Set);
+
    procedure Read_input (Rock_Store : out Rock_Stores.Map;
                          X_High, Y_High : out Ordinates) is
 
@@ -90,10 +97,65 @@ procedure December_14 is
                         First_Free := Y + 1; -- Presumed cube
                      end if; -- Rock_Store ((X, Y)) = Round
                   end if; -- Contains (Rock_Store, (X, Y))
-               end loop; --  Y in Ordinates range Ordinates'First .. Y_High
+               end loop; -- Y in Ordinates range Ordinates'First .. Y_High
             end loop; -- X in Ordinates range Ordinates'First .. X_High
-         when others =>
-            raise Program_Error with "unimplemented tilt direction";
+         when South =>
+            for X in Ordinates range Ordinates'First .. X_High loop
+               First_Free := Y_High;
+               for Y in reverse Ordinates range Ordinates'First .. Y_High loop
+                  if Contains (Rock_Store, (X, Y)) then
+                     if Rock_Store ((X, Y)) = Round then
+                        if Y < First_Free then
+                           insert (Rock_Store, (X, First_Free), Round);
+                           First_Free := @ - 1;
+                           Delete (Rock_Store, (X, Y));
+                        else
+                           First_Free := Y - 1;
+                        end if; -- Y < First_Free
+                     else
+                        First_Free := Y - 1; -- Presumed cube
+                     end if; -- Rock_Store ((X, Y)) = Round
+                  end if; -- Contains (Rock_Store, (X, Y))
+               end loop; -- Y in reverse Ordinates range Ordinates'First ...
+            end loop; -- X in Ordinates range Ordinates'First .. X_High
+         when East =>
+            for Y in Ordinates range Ordinates'First .. Y_High loop
+               First_Free := Ordinates'First;
+               for X in Ordinates range Ordinates'First .. X_High loop
+                  if Contains (Rock_Store, (X, Y)) then
+                     if Rock_Store ((X, Y)) = Round then
+                        if X > First_Free then
+                           insert (Rock_Store, (First_Free,Y), Round);
+                           First_Free := @ + 1;
+                           Delete (Rock_Store, (X, Y));
+                        else
+                           First_Free := X + 1;
+                        end if; -- X > First_Free
+                     else
+                        First_Free := X + 1; -- Presumed cube
+                     end if; -- Rock_Store ((X, Y)) = Round
+                  end if; -- Contains (Rock_Store, (X, Y))
+               end loop; -- X in Ordinates range Ordinates'First .. X_High
+            end loop; -- Y in Ordinates range Ordinates'First .. Y_High
+         when West =>
+            for Y in Ordinates range Ordinates'First .. Y_High loop
+               First_Free := X_High;
+               for X in reverse Ordinates range Ordinates'First .. X_High loop
+                  if Contains (Rock_Store, (X, Y)) then
+                     if Rock_Store ((X, Y)) = Round then
+                        if X > First_Free then
+                           insert (Rock_Store, (First_Free,Y), Round);
+                           First_Free := @ - 1;
+                           Delete (Rock_Store, (X, Y));
+                        else
+                           First_Free := X - 1;
+                        end if; -- X > First_Free
+                     else
+                        First_Free := X - 1; -- Presumed cube
+                     end if; -- Rock_Store ((X, Y)) = Round
+                  end if; -- Contains (Rock_Store, (X, Y))
+               end loop; -- X in reverse Ordinates range Ordinates'First ...
+            end loop; -- Y in Ordinates range Ordinates'First .. Y_High
       end case; -- Direction
    end Tilt;
 
@@ -111,6 +173,54 @@ procedure December_14 is
       return Result;
    end Load;
 
+   function Spin_Cycle (Rock_Store : in out Rock_Stores.Map;
+                        Y_High : in Ordinates) return Positive is
+
+      -- Has side effect of modifying Rock Store
+
+      use Round_Sets;
+      use Cycle_Maps;
+
+      function Build_Round_Set (Rock_Store : in Rock_Stores.Map)
+                                return Round_Sets.Set is
+
+         Result : Round_Sets.Set := Round_Sets.Empty_Set;
+
+      begin -- Build_Round_Set
+         for R in Iterate (Rock_Store) loop
+            if Element (R) = Round then
+               Insert (Result, Key (R));
+            end if; -- Element (R) = Round
+         end loop; -- Element (R) = Round
+         return Result;
+      end Build_Round_Set;
+
+      function Repeated (Cycle_Map : in out Cycle_Maps;
+                         Cycle : in Cycles;
+                         Round_Set : in Round_Sets) return Boolean is
+
+         -- Has side effect of updating Cycle_Map
+
+         Repeat : Boolean;
+         Cc : Cycle_Maps.Cursor := First (Cycle_Map);
+
+      begin -- Repeated
+         loop -- compare with all previous results
+            Repeat : Elemen (Cc) = Round_Set;
+            exit when Repeat;
+            Next (Cc);
+         end loop -- compare with all previous results
+         Insert (Cycle_Map, Cycle, Round_Set);
+         return Repeat;
+      end Repeated;
+
+      Cycle : Cycles := Cycles'Last;
+
+   begin -- Spin_Cycle
+      loop -- One Spin
+      end loop;
+   end Spin_Cycle; -- One Spin
+
    Rock_Store : Rock_Stores.Map;
    X_High, Y_High : Ordinates;
 
@@ -119,6 +229,7 @@ begin -- December_14
    Tilt (Rock_Store, North, X_High, Y_High);
    Put_Line ("Part one:" & Load (Rock_Store, Y_High)'Img);
    DJH.Execution_Time.Put_CPU_Time;
+   Read_input (Rock_Store, X_High, Y_High);
    Put_Line ("Part two:");
    DJH.Execution_Time.Put_CPU_Time;
 end December_14;
