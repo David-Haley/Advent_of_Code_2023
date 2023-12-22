ce75a00e67508cc03b5a3c311b33f72df34434df
David-Haley <david.haley@bigpond.com>
2023-12-23 00:19:45 +1000

    Day 20 Part 2, found the periods of all the imputs leading to rx low. tried a sloe LCM algorithm, then just took the product, which was correct.

--- src/december_20.adb
+++ src/december_20.adb
@@ -15,6 +15,8 @@ with DJH.Execution_Time; use DJH.Execution_Time;
 
 procedure December_20 is
 
+   subtype Long_Positive is Long_Long_Integer range 0 .. Long_Long_Integer'Last;
+
    subtype Pulses is Boolean;
 
    High : constant Pulses := True;
@@ -46,9 +48,23 @@ procedure December_20 is
    end record; -- State_Variables;
 
    package State_Variable_Maps is new
-      Ada.Containers.Indefinite_Ordered_Maps (Gate_Names, State_Variables);
+     Ada.Containers.Indefinite_Ordered_Maps (Gate_Names, State_Variables);
    use State_Variable_Maps;
 
+   type Messages is record
+      Source, Destination : Gate_Names;
+      Pulse : Pulses;
+   end record; -- Messages
+
+   package Q_Interface is new
+     Ada.Containers.Synchronized_Queue_Interfaces (Messages);
+   use Q_Interface;
+
+   package Message_Queues is new
+     Ada.Containers.Unbounded_Synchronized_Queues (Q_Interface);
+   use Message_Queues;
+
+   Last_Machine : constant Gate_Names := To_Unbounded_String ("rx");
 
    procedure Read_input (State_Variable_Map : out State_Variable_Maps.Map) is
 
@@ -70,7 +86,6 @@ procedure December_20 is
          Open (Input_File, In_File, Argument(1));
       end if; -- Argument_Count = 0
       Clear (State_Variable_Map);
-      Put_Line ("Pass 1");
       while not End_Of_File (Input_File) loop
          Get_Line (Input_File, Text);
          case Element (Text, 1) is
@@ -99,7 +114,6 @@ procedure December_20 is
          end; -- State_Variable declaration block
       end loop; -- not End_Of_File (Input_File)
       Reset (Input_File);
-      Put_Line ("Pass 2");
       while not End_Of_File (Input_File) loop
          Get_Line (Input_File, Text);
          Start_At := 1;
@@ -132,20 +146,7 @@ procedure December_20 is
    end Read_input;
 
    procedure Push_Button (State_Variable_Map : in out State_Variable_Maps.Map;
-                         Count_High, Count_Low : in out Natural) is
-
-      type Messages is record
-         Source, Destination : Gate_Names;
-         Pulse : Pulses;
-      end record; -- Messages
-
-      package Q_Interface is new
-        Ada.Containers.Synchronized_Queue_Interfaces (Messages);
-      use Q_Interface;
-
-      package Message_Queues is new
-        Ada.Containers.Unbounded_Synchronized_Queues (Q_Interface);
-      use Message_Queues;
+                          Count_High, Count_Low : in out Natural) is
 
       Message_Queue : Message_Queues.Queue;
 
@@ -160,11 +161,10 @@ procedure December_20 is
       Anded : Pulses;
 
    begin -- Push_Button
-      Send (To_Unbounded_String ("button"), To_Unbounded_String ("broadcaster"),
-            Low);
+      Send (To_Unbounded_String ("button"),
+            To_Unbounded_String ("broadcaster"), Low);
       while Message_Queue.Current_Use > 0 loop
          Message_Queue.Dequeue (Current_Message);
-         --  Put_Line (Current_Message'Img);
          if Current_Message.Pulse then
             Count_High := @ + 1;
          else
@@ -173,7 +173,7 @@ procedure December_20 is
          case State_Variable_Map (Current_Message.Destination).Gate is
             when Broadcaster =>
                for O in Iterate (State_Variable_Map
-                 (Current_Message.Destination).Output_List) loop
+                                 (Current_Message.Destination).Output_List) loop
                   Send (Current_Message.Destination, Element (O),
                         Current_Message.Pulse);
                end loop; -- O in Iterate (State_Variable_Map
@@ -183,7 +183,8 @@ procedure December_20 is
                     not State_Variable_Map (Current_Message.Destination).
                     FF_State;
                   for O in Iterate (State_Variable_Map
-                    (Current_Message.Destination).Output_List) loop
+                                    (Current_Message.Destination).
+                                      Output_List) loop
                      Send (Current_Message.Destination, Element (O),
                            State_Variable_Map (Current_Message.Destination).
                              FF_State);
@@ -199,7 +200,7 @@ procedure December_20 is
                   Anded := @ and Element (I);
                end loop; -- I in
                for O in Iterate (State_Variable_Map
-                 (Current_Message.Destination).Output_List) loop
+                                 (Current_Message.Destination).Output_List) loop
                   Send (Current_Message.Destination, Element (O), not Anded);
                end loop; -- O in Iterate (State_Variable_Map
             when Output =>
@@ -211,6 +212,132 @@ procedure December_20 is
       end loop; -- Message_Queue.Current_Use > 0
    end Push_Button;
 
+   function Presses_Two (State_Variable_Map : in out State_Variable_Maps.Map)
+                         return Long_Positive is
+
+      Message_Queue : Message_Queues.Queue;
+
+      procedure Send (Source, Destination : in Gate_Names;
+                      Pulse : in Pulses) is
+
+      begin -- Send
+         Message_Queue.Enqueue ((Source, Destination, Pulse));
+      end Send;
+
+      package Count_Lists is new Ada.Containers.Doubly_Linked_Lists (Natural);
+      use Count_Lists;
+
+      package High_Counts is new
+        Ada.Containers.Ordered_Maps (Gate_Names, Count_Lists.List);
+      use High_Counts;
+
+      procedure Build (State_Variable_Map : in State_Variable_Maps.Map;
+                       Nand_Gate : out Gate_Names;
+                       High_Count : out High_Counts.Map) is
+
+         -- Finds the name of the nand gate and intialses the High_Count
+         -- data structure;
+
+         Output_List : Output_Lists.List;
+
+      begin -- Build
+         Clear (High_Count);
+         for G in Iterate (State_Variable_Map) loop
+            Output_List := Element (G).Output_List;
+            for O in Iterate (Output_List) loop
+               if Element (O) = Last_Machine then
+                  Nand_Gate := Key (G);
+               end if; -- Element (O) = Last_Machine
+            end loop; -- O in .Iterate (Output_List)
+         end loop; -- G in Iterate (State_Variable_Map)
+         for I in Iterate (State_Variable_Map (Nand_Gate).Input_Map) loop
+            Insert (High_Count, Key (I), Count_Lists.Empty_List);
+         end loop; -- I in Iterate (State_Variable_Map (Nand_Gate).Input_Map)
+      end Build;
+
+      Function All_High (High_Count : in High_Counts.Map) return Boolean is
+
+         -- Wait for atleast two high pulses fot each input to confirm
+         -- first and second cycles are the same length.
+
+         Result : Boolean := True;
+
+      begin -- All_High
+         for H in iterate (High_Count) loop
+            Result := @ and Length (Element (H)) >= 2;
+         end loop; -- H in iterate (High_Count)
+         return Result;
+      end All_High;
+
+      Count_High, Count_Low, Press_Count : Natural := 0;
+      High_Count : High_Counts.Map;
+      Nand_Gate : Gate_Names;
+      Current_Message : Messages;
+      Anded : Pulses;
+      Product : Long_Positive := 1;
+
+   begin -- Presses_Two
+      Build (State_Variable_Map, Nand_Gate, High_Count);
+      while not All_High (High_Count) loop
+         Press_Count := @ + 1;
+         Send (To_Unbounded_String ("button"),
+               To_Unbounded_String ("broadcaster"), Low);
+         while Message_Queue.Current_Use > 0 loop
+            Message_Queue.Dequeue (Current_Message);
+            -- Capture the count when high messages are sent
+            If Current_Message.Destination = Nand_Gate and
+              Current_Message.Pulse then
+               append (High_Count (Current_Message.Source), Press_Count);
+            end if; -- Current_Message.Destination := Nand
+            case State_Variable_Map (Current_Message.Destination).Gate is
+            when Broadcaster =>
+               for O in Iterate (State_Variable_Map
+                                 (Current_Message.Destination).
+                                   Output_List) loop
+                  Send (Current_Message.Destination, Element (O),
+                        Current_Message.Pulse);
+               end loop; -- O in Iterate (State_Variable_Map
+            when Flip_Flop =>
+               if not Current_Message.Pulse then
+                  State_Variable_Map (Current_Message.Destination).FF_State :=
+                    not State_Variable_Map (Current_Message.Destination).
+                    FF_State;
+                  for O in Iterate (State_Variable_Map
+                                    (Current_Message.
+                                         Destination).Output_List) loop
+                     Send (Current_Message.Destination, Element (O),
+                           State_Variable_Map (Current_Message.Destination).
+                             FF_State);
+                  end loop; -- O in Iterate (State_Variable_Map
+               end if; -- not Current_Message.Pulse
+            when Conjunction =>
+               Anded := High;
+               State_Variable_Map (Current_Message.Destination).
+                 Input_Map (Current_Message.Source) := Current_Message.Pulse;
+               for I in
+                 Iterate (State_Variable_Map (Current_Message.Destination).
+                              Input_Map) loop
+                  Anded := @ and Element (I);
+               end loop; -- I in
+               for O in Iterate (State_Variable_Map
+                                 (Current_Message.Destination).
+                                   Output_List) loop
+                  Send (Current_Message.Destination, Element (O), not Anded);
+               end loop; -- O in Iterate (State_Variable_Map
+            when Output =>
+               State_Variable_Map (Current_Message.Destination).Received :=
+                 @ and Current_Message.Pulse;
+               -- Latches low, allows for it being set hign again before the
+               -- Message_Queue empties.
+            end case; -- State_Variable_Map (Current_Message.Destination).Gate
+         end loop; -- Message_Queue.Current_Use > 0
+      end loop; -- not All_High (High_Count)
+      for H in Iterate (High_Count) loop
+         Product := @ * Long_Positive (First_Element (Element (H)));
+      end loop; -- H in Iterate (High_Count)
+      return Product;
+   end Presses_Two;
+
    State_Variable_Map : State_Variable_Maps.Map;
    Count_High, Count_Low, Press_Count : Natural := 0;
 
@@ -221,12 +348,7 @@ begin -- December_20
    end loop; -- P in Positive range 1 .. 1000
    Put_Line ("Part one:" & Natural'Image (Count_High * Count_Low));
    DJH.Execution_Time.Put_CPU_Time;
-   Count_High := 0;
-   Count_Low := 0;
-   while State_Variable_Map (To_Unbounded_String ("rx")).Received loop
-      Press_Count := @ + 1;
-      Push_Button (State_Variable_Map, Count_High, Count_Low);
-   end loop; -- State_Variable_Map (To_Unbounded_String ("rx")).Received
-   Put_Line ("Part two:" & Press_Count'img);
+   Read_input (State_Variable_Map);
+   Put_Line ("Part two:" & Presses_Two (State_Variable_Map)'img);
    DJH.Execution_Time.Put_CPU_Time;
 end December_20;
