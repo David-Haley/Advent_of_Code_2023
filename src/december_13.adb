9072316ec0311bc2cf55efc5e8fb559b16ac0db9
David-Haley <david.haley@bigpond.com>
2024-01-17 00:30:31 +1000

    Day 13, work in progress, need to find unique reflection?

--- src/december_13.adb
+++ src/december_13.adb
@@ -32,7 +32,7 @@ procedure December_13 is
    package Mirror_Lists is new Ada.Containers.Doubly_Linked_Lists (Ordinates);
 
    type Smudges is record
-      Object, Reflection, Position : Ordinates;
+      Object, Reflection : Ordinates;
    end record; -- Smudges
 
    package Smudge_Lists is new Doubly_Linked_Lists (Smudges);
@@ -87,6 +87,7 @@ procedure December_13 is
       O, R : Ordinates;
 
    begin -- Find_Mirror
+      Position := 1;
       for V in Iterate (Vector) loop
          Vn := Next (V);
          if Vn /= Vectors.No_Element and then Key (V) + 1 = Key (Vn) and then
@@ -99,12 +100,14 @@ procedure December_13 is
       Found := False;
       while Mc /= Mirror_Lists.No_Element and not Found loop
          Found := True;
-         Position := Element (MC);
+         Position := Element (Mc);
          O := Position;
          R := Position + 1;
          loop -- Test reflection
             If Contains (Vector, O) and Contains (Vector, R) then
                Found := @ and Element (Vector, O) = Element (Vector, R);
+            else
+               exit;
             end if; -- Contains (Vector, O) and Contains (Vector, R)
             exit when O = 1;
             O := @ - 1;
@@ -114,54 +117,60 @@ procedure December_13 is
       end loop; -- Mc /= Mirror_Lists.No_Element and not Found
    end Find_Mirror;
 
-   procedure  Find_Mirror_2 (Vector : in Vectors.Map;
+   procedure  Find_Mirror_2 (Vector_In : in Vectors.Map;
                              Found : out Boolean;
                              Position : out Ordinates) is
 
       use Smudge_Lists;
-      Difference : Lines.Set;
+      Vector : Vectors.Map;
       Vn : Vectors.Cursor;
       Smudge_List : Smudge_Lists.List := Smudge_Lists.Empty_List;
       Sc : Smudge_Lists.Cursor;
-      O, R : Ordinates;
+      Position_O, Position_R, Part_1_Position : Ordinates;
+      Found_O, Found_R, Part_1_Found : Boolean;
 
    begin -- Find_Mirror_2
+      Vector := Copy (Vector_In);
       for V in Iterate (Vector) loop
          Vn := Next (V);
          while Vn /= Vectors.No_Element loop
-            Difference := Symmetric_Difference (Element (V), Element (Vn));
-            if Length (Difference) = 1 then
-               Append (Smudge_List, (Object => Key (V),
-                                     Reflection => Key (Vn),
-                                     Position => First_Element (Difference)));
-            end if; -- Length (Difference) = 1
+            if Length (Symmetric_Difference (Element (V), Element (Vn))) = 1
+            then
+               Append (Smudge_List, (Key (V), Key (Vn)));
+            end if; --  Length (Symmetric_Difference (Element (V) ...
             Next (Vn);
          end loop; -- Vn /= Vectors.No_Element
       end loop; -- V in Iterate (Vector)
       Sc := First (Smudge_List);
+      Put_Line ("Smudge_List: " & Smudge_List'Img);
+      Find_Mirror (Vector, Part_1_Found, Part_1_Position);
+      Put_Line ("Part_1: " & Part_1_Found'Img & Part_1_Position'Img);
       Found := False;
       while Sc /= Smudge_Lists.No_Element and not Found loop
-         Found := True;
-         Position := (Element (Sc).Object + Element (Sc).Reflection) / 2;
-         O := Position;
-         R := Position + 1;
-         loop -- Test reflection
-            If Contains (Vector, O) and Contains (Vector, R) then
-               Difference := Symmetric_Difference (Element (Vector, O),
-                                                   Element (Vector, R));
-               Found := @ and (Element (Vector, O) = Element (Vector, R)
-                               or else (O = Element (Sc).Object and
-                                          R = Element (Sc).Reflection and
-                                          First_Element (Difference) =
-                                          Element (Sc).Position));
-            end if; -- Contains (Vector, O) and Contains (Vector, R)
-            exit when O = 1;
-            O := @ - 1;
-            R := @ + 1;
-         end loop; -- Test reflection
+         Vector := Copy (Vector_In);
+         Put (Element (Sc)'Img);
+         Vector (Element (Sc).Object) := Vector (Element (Sc).Reflection);
+         Find_Mirror (Vector, Found_O, Position_O);
+         if Part_1_Found and then Position_O = Part_1_Position then
+            Found_O := False;
+         end if; -- Part_1_Found and then Position_O = Part_1_Position
+         Put (Found_O'Img & Position'Img);
+         Vector := Copy (Vector_In);
+         Vector (Element (Sc).Reflection) := Vector (Element (Sc).Object);
+         Find_Mirror (Vector, Found_R, Position_R);
+         if Part_1_Found and then Position_R = Part_1_Position then
+            Found_R := False;
+         end if; -- Part_1_Found and then Position_R = Part_1_Position
+         Found := Found_O or Found_R;
+         Put_Line (Found_R'Img & Position'Img);
          Next (Sc);
       end loop; -- Sc /= Smudge_Lists.No_Element and not Found
-   end  Find_Mirror_2;
+      if Found_O then
+         Position := Position_O;
+      elsif Found_R then
+         Position := Position_R;
+      end if; -- Found_O
+   end Find_Mirror_2;
 
    Patch_Store : Patch_Stores.List;
    Found : Boolean;
