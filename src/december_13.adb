92df4e6bca0db4daa28d4d11f53724f3191593a1
David-Haley <david.haley@bigpond.com>
2024-01-17 12:57:30 +1000

    Day 13, both parts, in part 2 had to allow for multiple mirrors and remove a smudge resulting in a unique mirror other than that found in part 1.

--- src/december_13.adb
+++ src/december_13.adb
@@ -24,6 +24,8 @@ procedure December_13 is
 
    type Patches is record
       Column, Row : Vectors.Map;
+      -- two copies of the patch data are storred to allow efficient checking
+      -- for the existance of vertical or horizontal mirrors.
    end record; -- Patches
 
    package Patch_Stores is new Ada.Containers.Doubly_Linked_Lists (Patches);
@@ -56,16 +58,18 @@ procedure December_13 is
          Clear (Patch.Row);
          Get_Line (Input_File, Text);
          Y := 1;
+         -- Note all rows and columns must be stored even if they don't comtain
+         -- rocks. Empty sets need to be considered in reflections.
          While Length (Text) > 0 and not End_Of_File (Input_File) loop
+            if not Contains (Patch.Row, Y) then
+               Include (Patch.Row, Y, Lines.Empty_Set);
+            end if; -- not Contains (Patch.Column, X)
             for X in Ordinates range Ordinates'First .. Length (Text) loop
+               if not Contains (Patch.Column, X) then
+                  Include (Patch.Column, X, Lines.Empty_Set);
+               end if; -- not Contains (Patch.Column, X)
                if Element (Text, X) = '#' then
-                  if not Contains (Patch.Column, X) then
-                     Include (Patch.Column, X, Lines.Empty_Set);
-                  end if; -- not Contains (Patch.Column, X)
                   Include (Patch.Column (X), Y);
-                  if not Contains (Patch.Row, Y) then
-                     Include (Patch.Row, Y, Lines.Empty_Set);
-                  end if; -- not Contains (Patch.Column, X)
                   Include (Patch.Row (Y), X);
                end if; -- Elwment (Text, X) = '#'
             end loop; -- X in Ordinates range Ordinates'First .. Length (Text)
@@ -78,16 +82,21 @@ procedure December_13 is
    end Read_input;
 
    procedure Find_Mirror (Vector : in Vectors.Map;
-                          Found : out Boolean;
-                          Position : out Ordinates) is
+                          Mirror_Set : out Lines.Set) is
+
+      -- Finds the set of mirrors by intiially locking at adjacent pairs of
+      -- lines that match and then checking that lines working away also match.
+
       use Mirror_Lists;
       Vn : Vectors.Cursor;
       Mirror_List : Mirror_Lists.List := Mirror_Lists.Empty_List;
       Mc : Mirror_Lists.Cursor;
       O, R : Ordinates;
+      Found : Boolean;
 
    begin -- Find_Mirror
-      Position := 1;
+      Clear (Mirror_Set);
+      -- Find matching pairs
       for V in Iterate (Vector) loop
          Vn := Next (V);
          if Vn /= Vectors.No_Element and then Key (V) + 1 = Key (Vn) and then
@@ -96,42 +105,44 @@ procedure December_13 is
             Append (Mirror_List, Key(V));
          end if; -- Vn /= Vectors.No_Element and then Key (V) + 1 = Key (Vn) ...
       end loop; -- V in Iterate (Vector)
+      -- Check that this is truly a mirror by comparing the rest of the patch.
       Mc := First (Mirror_List);
-      Found := False;
-      while Mc /= Mirror_Lists.No_Element and not Found loop
+      while Mc /= Mirror_Lists.No_Element loop
          Found := True;
-         Position := Element (Mc);
-         O := Position;
-         R := Position + 1;
+         O := Element (Mc);
+         R := Element (Mc) + 1;
          loop -- Test reflection
-            If Contains (Vector, O) and Contains (Vector, R) then
-               Found := @ and Element (Vector, O) = Element (Vector, R);
-            else
-               exit;
-            end if; -- Contains (Vector, O) and Contains (Vector, R)
-            exit when O = 1;
+            Found := @ and Element (Vector, O) = Element (Vector, R);
+            exit when O = Ordinates'First or R = Last_Key (Vector);
             O := @ - 1;
             R := @ + 1;
          end loop; -- Test reflection
+         if Found then
+            -- Store the location of the mirror, can be more than 1, e.g. second
+            -- patch in example in part 2.
+            Include (Mirror_Set, Element (Mc));
+         end if; -- Found
          Next (Mc);
-      end loop; -- Mc /= Mirror_Lists.No_Element and not Found
+      end loop; -- Mc /= Mirror_Lists.No_Element
    end Find_Mirror;
 
    procedure  Find_Mirror_2 (Vector_In : in Vectors.Map;
-                             Found : out Boolean;
-                             Position : out Ordinates) is
+                             Mirror_Set : out Lines.Set) is
+
+      -- Find pairs of lines with one difference. Then check that this produces
+      -- one or more mirrors that are different from the part one mirror.
 
       use Smudge_Lists;
       Vector : Vectors.Map;
       Vn : Vectors.Cursor;
       Smudge_List : Smudge_Lists.List := Smudge_Lists.Empty_List;
       Sc : Smudge_Lists.Cursor;
-      Position_O, Position_R, Part_1_Position : Ordinates;
-      Found_O, Found_R, Part_1_Found : Boolean;
+      Mirror_Set_O, Mirror_Set_R, Part_1_Mirror_Set : Lines.Set;
 
    begin -- Find_Mirror_2
-      Vector := Copy (Vector_In);
-      for V in Iterate (Vector) loop
+      Clear (Mirror_Set);
+      -- Find potential smudges.
+      for V in Iterate (Vector_In) loop
          Vn := Next (V);
          while Vn /= Vectors.No_Element loop
             if Length (Symmetric_Difference (Element (V), Element (Vn))) = 1
@@ -140,69 +151,57 @@ procedure December_13 is
             end if; --  Length (Symmetric_Difference (Element (V) ...
             Next (Vn);
          end loop; -- Vn /= Vectors.No_Element
-      end loop; -- V in Iterate (Vector)
+      end loop; -- V in Iterate (Vector_In)
+      -- Find mirrors resulting from correction of the smudge. Both options of
+      -- removing a rock and inserting a rock are seperatly tested.
       Sc := First (Smudge_List);
-      Put_Line ("Smudge_List: " & Smudge_List'Img);
-      Find_Mirror (Vector, Part_1_Found, Part_1_Position);
-      Put_Line ("Part_1: " & Part_1_Found'Img & Part_1_Position'Img);
-      Found := False;
-      while Sc /= Smudge_Lists.No_Element and not Found loop
+      while Sc /= Smudge_Lists.No_Element loop
          Vector := Copy (Vector_In);
-         Put (Element (Sc)'Img);
          Vector (Element (Sc).Object) := Vector (Element (Sc).Reflection);
-         Find_Mirror (Vector, Found_O, Position_O);
-         if Part_1_Found and then Position_O = Part_1_Position then
-            Found_O := False;
-         end if; -- Part_1_Found and then Position_O = Part_1_Position
-         Put (Found_O'Img & Position'Img);
+         Find_Mirror (Vector, Mirror_Set_O);
+         Mirror_Set := Union (Mirror_Set, Mirror_Set_O);
          Vector := Copy (Vector_In);
          Vector (Element (Sc).Reflection) := Vector (Element (Sc).Object);
-         Find_Mirror (Vector, Found_R, Position_R);
-         if Part_1_Found and then Position_R = Part_1_Position then
-            Found_R := False;
-         end if; -- Part_1_Found and then Position_R = Part_1_Position
-         Found := Found_O or Found_R;
-         Put_Line (Found_R'Img & Position'Img);
+         Find_Mirror (Vector, Mirror_Set_R);
+         Mirror_Set := Union (Mirror_Set, Mirror_Set_R);
          Next (Sc);
-      end loop; -- Sc /= Smudge_Lists.No_Element and not Found
-      if Found_O then
-         Position := Position_O;
-      elsif Found_R then
-         Position := Position_R;
-      end if; -- Found_O
+      end loop; -- Sc /= Smudge_Lists.No_Element
+      -- Remove the mirror if it is the same as part 1
+      Find_Mirror (Vector_In, Part_1_Mirror_Set);
+      if Length (Part_1_Mirror_Set) = 1 then
+         Exclude (Mirror_Set, First_Element (Part_1_Mirror_Set));
+      end if; -- Length (Part_1_Mirror_Set) = 1
    end Find_Mirror_2;
 
    Patch_Store : Patch_Stores.List;
-   Found : Boolean;
-   Position : Ordinates;
+   Mirror_Set : Lines.Set;
    Sum : Natural;
-   Cleaned_Patch : Patches;
 
 begin -- December_13
    Read_input (Patch_Store);
    Sum := 0;
    for P in Iterate (Patch_Store) loop
-      Find_Mirror (Patch_Store (P).Row, Found, Position);
-      if Found then
-         Sum := @ + 100 * Position;
-      end if; -- Found
-      Find_Mirror (Patch_Store (P).Column, Found, Position);
-      if Found then
-         Sum := @ + Position;
-      end if; -- Found
+      Find_Mirror (Patch_Store (P).Row, Mirror_Set);
+      if Length (Mirror_Set) = 1 then
+         Sum := @ + 100 * First_Element (Mirror_Set);
+      end if; -- Length (Mirror_Set) = 1
+      Find_Mirror (Patch_Store (P).Column, Mirror_Set);
+      if Length (Mirror_Set) = 1 then
+         Sum := @ + 1 * First_Element (Mirror_Set);
+      end if; -- Length (Mirror_Set) = 1
    end loop; -- P in Iterate (Patch_Store)
    Put_Line ("Part one:" & Sum'Img);
    DJH.Execution_Time.Put_CPU_Time;
    Sum := 0;
    for P in Iterate (Patch_Store) loop
-      Find_Mirror_2 (Patch_Store (P).Row, Found, Position);
-      if Found then
-         Sum := @ + 100 * Position;
-      end if; -- Found
-      Find_Mirror_2 (Patch_Store (P).Column, Found, Position);
-      if Found then
-         Sum := @ + Position;
-      end if; -- Found
+      Find_Mirror_2 (Patch_Store (P).Row, Mirror_Set);
+      if Length (Mirror_Set) = 1 then
+         Sum := @ + 100 * First_Element (Mirror_Set);
+      end if; -- Length (Mirror_Set) = 1
+      Find_Mirror_2 (Patch_Store (P).Column, Mirror_Set);
+      if Length (Mirror_Set) = 1 then
+         Sum := @ + 1 * First_Element (Mirror_Set);
+      end if; -- Length (Mirror_Set) = 1
    end loop; -- P in Iterate (Patch_Store)
    Put_Line ("Part two:" & Sum'Img);
    DJH.Execution_Time.Put_CPU_Time;
